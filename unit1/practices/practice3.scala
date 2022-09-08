93e63700c315b8fcf2770156dbd2eb4b25f72480
adrianacuna <ing.adrianacuna@gmail.com>
2022-09-07 19:51:31 -0700

    Adding the excersice number 2 to verify the value of the result is divisble with 2

--- unit1/practices/practice3.scala
+++ unit1/practices/practice3.scala
@@ -2,3 +2,13 @@
 //1. Desarrollar un algoritmo en scala que calcule el radio de un circulo
 val radio = 25
 val diametro = 2 * longitud
+//2. Desarrollar un algoritmo en scala que me diga si un numero es primo
+val numero = 10
+val esPrimo = numero % 2
+if (esPrimo == 0) {
+  val primo = s"El numero ${numero} es par"
+  println(primo)
+} else {
+  val primo = s"El numero ${numero} es primo"
+  println(primo)
+}
