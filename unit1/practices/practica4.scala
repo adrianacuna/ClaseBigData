bd0ae6aaf29b699eee65289db32e62e8ea8c2df7
josuejcamacho9 <josue.camacho@uabc.edu.mx>
2022-09-10 19:42:38 -0700

    Added second excercise to pratice 4, Add to list Lista 5 new elements

--- unit1/practices/practica4.scala
+++ unit1/practices/practica4.scala
@@ -1,3 +1,7 @@
 // 1.Crea una lista llamada "lista" con los elementos "rojo", "blanco", "negro"
 var lista = List("Rojo", "Blanco", "Negro")
 lista
+
+//2. Añadir 5 elementos mas a "lista" "verde" ,"amarillo", "azul", "naranja", "perla"
+lista.++=(List("verde", "amarillo", "azul","naranja","perla"))
+println(lista)
